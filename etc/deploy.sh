d3db4c847eca2fcb68363232fc237d3e25c506ea
aghast <ah08010-github@yahoo.com>
2019-09-29 17:00:42 -0400

    Added etc/ directory

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,41 +1,137 @@
 #!/bin/sh
 
-if [ -z "$DOTFILES" ]
-then
-	printf "ERROR: The DOTFILES environment variable is not set!\n" >&2
-	printf "Set it to the root of the dotfiles project.\n\n" >&2
-	exit 1
-fi
+## CTAGS
 
-ETC_PATH=${ETC_PATH:-$(dirname "$(readlink -f "$0")")}
+deploy_ctags() {
+	local ctags_file="$DOTFILES/etc/ctags"
 
-get_shellname() {
-	shell="${1:-$(getent passwd "$LOGNAME" | cut -d: -f7)}"
-	whatshell=$("$shell" "$DOTFILES/etc/whatshell.sh")
-	set -- $whatshell
+	require_file "$ctags_file" || return 1
 
-	whatshell="$1"
-	if [ "$whatshell" = "ash" -a "$(uname -s)" = 'Linux'* ]
-	then
-		whatshell=dash
-	fi
-
-	printf "$whatshell"
+	case "$1" in
+	links)	
+		symlink "$ctags_file" "$HOME/.ctags"
+		;;
+	copies)	
+		cp -ab "$ctags_file" "$HOME/ctags.cnf" \
+		|| ERROR "Failed to copy ~/ctags.cnf <- $ctags_file" 
+		;;
+	*) 
+		ERROR "Invalid link/copy type: '$1'" 
+		;;
+	esac
 }
 
-deploy() {
-	local shellname=""	# Not POSIX. Comment it out if you need to.
-	local deploy_script=""
+deploy_git() {
+	local config_file="$DOTFILES/etc/gitconfig"
+	require_file "$config_file" || return 1
 
-	shellname=$(get_shellname $1)
-	deploy_script="$DOTFILES/$shellname/deploy-$shellname.sh"
-	
-	if [ -f "$deploy_script" ]
+	local ignore_file="$DOTFILES/etc/cvsignore"
+	require_file "$ignore_file" || return 1
+
+	case "$1" in
+	links)	
+		symlink "$config_file" "$HOME/.gitconfig"
+		symlink "$ignore_file" "$HOME/.cvsignore"
+		;;
+	copies)	
+		cp -ab "$config_file" "$HOME/.gitconfig" \
+		|| ERROR "Failed to copy ~/.gitconfig <- $config_file" 
+		cp -ab "$ignore_file" "$HOME/.cvsignore" \
+		|| ERROR "Failed to copy ~/.cvsignore <- $ignore_file" 
+		;;
+	*) 
+		ERROR "Invalid link/copy type: '$1'" 
+		;;
+	esac
+}
+
+#################################################
+
+ERROR() {
+	printf "ERROR: " >&2
+	printf "$*\n" >&2
+	return 1
+}
+
+links_or_copies() {
+	case "$(uname -s)" in
+	*BSD)		printf "links\n"	;; # FreeBSD or NetBSD (or ...?)
+	Linux)		printf "links\n"	;; # Linux
+	CYGWIN_NT*)	printf "copies\n"	;; # cygwin
+	Interix)	printf "copies\n"	;; # Windows Services for Unix
+	Windows_NT)	printf "copies\n"	;; # busybox-w32
+	MS-DOS)				   # djgpp
+		printf "ERROR: Don't know how to deploy to MS-DOS!\n" >&2
+		return 1
+		;;  
+	GNU*)				   # Debian GNU/HURD
+		printf "ERROR: Don't know how to deploy on HURD!\n\n" >&2
+		return 1
+		;;
+	*)				   # "other"
+		printf "ERROR: Don't know how to deploy on $(uname -s)!\n" >&2
+		return 1
+		;;
+	esac
+
+	return 0
+}
+
+
+main() {
+	if [ -z "$DOTFILES" ] || [ ! -d "$DOTFILES" ]
 	then
-		. "$deploy_script"
+		printf "ERROR: \$DOTFILES is not set, or directory missing." >&2
+		return 1
+	fi
+
+	local linktype="$(links_or_copies)"
+
+	if [ -z "$linktype" ] 
+	then
+		return 1
+	fi
+
+	deploy_ctags $linktype
+	deploy_git $linktype
+}
+
+require_file() {
+	if [ -f "$1" ]
+	then
+		: return true
 	else
-		printf "ERROR: No $deploy_script script found\n\n" >&2
+		ERROR "'$1' missing"
 	fi
 }
 
-deploy $1
+symlink() {
+	local target="$1"	# Pointed to by link
+	local link="$2"		# Link -> target
+
+	if [ -e "$link" ]
+	then
+		if [ -h "$link" ]
+		then
+			rm "$link" \
+			|| ERROR "Could not remove existing symlink '$link'"
+		else
+			ERROR "File '$link' blocks creation of symlink."
+		fi
+
+		if [ $? -ne 0 ]
+		then
+			return 1
+		fi
+	fi
+
+	if ln -s "$target" "$link"
+	then
+		return 0
+	else
+		ERROR "Failed to link '$link' -> '$target'"
+		return 1
+	fi
+}
+
+main
