0c946f2f3828466b0a03857a37f5e7869231404a
aghast <ah08010-github@yahoo.com>
2019-10-07 21:13:42 -0400

    Merge deploy.sh -> etc/deploy.sh

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+ETC_PATH=${ETC_PATH:-$(dirname "$(readlink -f "$0")")}
+
+# FIXME: Switch this to computing dotfiles from etc_path, above.
+if [ -z "$DOTFILES" ]
+then
+	DOTFILES="${ETC_PATH%/*}"
+	echo "DOTFILES=$DOTFILES"
+	exit 1
+fi
+
 . "$DOTFILES/etc/deploy-functions.sh"
 
 deploy_git() {
@@ -44,6 +54,51 @@ deploy_inputrc() {
 	esac
 }
 
+deploy_shell() {
+	local shellname=""	# Not POSIX. Comment it out if you need to.
+	local deploy_script=""
+
+	shellname=$(get_shellname $1)
+	deploy_script="$DOTFILES/$shellname/deploy-$shellname.sh"
+	
+	if [ -f "$deploy_script" ]
+	then
+		. "$deploy_script"
+	else
+		printf "ERROR: No $deploy_script script found\n\n" >&2
+	fi
+}
+
+#
+# Deploy a subsystem stored in a subdirectory below $DOTFILES.
+#
+deploy_subdir() {
+	local subdir="${1}"
+
+	if [ -z "$subdir" ] || [ ! -d "$subdir" ]
+	then
+		printf "Usage: deploy_subdir DIR" >&2
+		return 1
+	fi
+
+	( cd "$subdir" && ./etc/deploy.sh )
+}
+
+get_shellname() {
+	shell="${1:-$(getent passwd "$LOGNAME" | cut -d: -f7)}"
+	whatshell=$("$shell" "${ETC_PATH}/whatshell.sh")
+	set -- $whatshell
+
+	whatshell="$1"
+	if [ "$whatshell" = "ash" -a "$(uname -s)" = 'Linux'* ]
+	then
+		whatshell=dash
+	fi
+
+	printf "$whatshell"
+}
+
+
 main() {
 	require_dotfiles
 	local linktype="$(links_or_copies)"
@@ -53,9 +108,10 @@ main() {
 		return 1
 	fi
 
-	deploy_ctags $linktype
 	deploy_git $linktype
 	deploy_inputrc $linktype
+	deploy_shell $1
+	deploy_subdir "vim"
 }
 
 main
